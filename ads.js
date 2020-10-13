cb0772875e91f625a7140eaee673444b03a92203
agou <2833769658@qq.com>
2020-10-13 14:43:44 +0800

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,8 +1,6 @@
 ad_types=[/* 正方形 */"7888411968", /* 横向 */"8287867219", /* 资料站信息流 */"2505400067"];
 function showad(type, width, height, others){
 	
-	document.cookie="username=John Doe; expires=Thu, 18 Dec 2043 12:00:00 GMT; path=/";
-	// document.write(document.cookie);
 	if (getCookie("noad")==""){
 		
 		document.writeln("<script async src=\'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\'></script><ins class=\'adsbygoogle\'style=\'display:block;width:"+width+";height:"+height+";"+others+"\'data-ad-client=\'ca-pub-5639925822995109\'data-ad-slot=\'"+ad_types[type]+"\'data-full-width-responsive=\'false\' s></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script><div class='noblock button' style='display: none; width: 100%; margin: 10px auto;'><p style=' line-height: 2.0;padding: 20px;'>看起来你使用了广告拦截器。殊不知，广告是我们网站重要的收入来源之一。即使你不想在网站上看到广告，又何苦使用这样的方式呢？我们早就在junbo.wang/settings提供了关闭通道，你可以到那里关闭广告。如果你不想看到此通知，请一并把这个提示屏蔽吧。</p></div>");		
@@ -12,7 +10,7 @@ function showad(type, width, height, others){
 	}
 	else{
 		
-		// document.write("无广告");
+		document.write("无广告");
 		
 	}
 	
@@ -21,14 +19,14 @@ function showad(type, width, height, others){
 
 function closead(){
 		
-	document.cookie="noad=open; max-age=2626560";
+	document.cookie="noad=open; max-age=2626560;path=/";
 
 	
 }
 
 function openad(){
 		
-	document.cookie="noad=; max-age=0";
+	document.cookie="noad=; max-age=0;path=/";
 
 	
 }
